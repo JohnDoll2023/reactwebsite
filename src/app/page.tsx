61bd9df97d11ae6d8218c4f53bd0ffb8e203a3b4
John Doll <jdollmu@gmail.com>
2025-03-10 20:22:52 -0400

    progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,25 +19,32 @@ export default function Home() {
   }, []);
   return (
     <main className="flex-1 mt-3">
-    <div className="flex flex-row">
-        <div>
-            <Image
-                className="rounded-md"
-                src="/doll.jpg"
-                alt="me, John Doll"
-                width={600}
-                height={600}
-                priority
-            />
+      <div className="flex flex-col max-h-screen">
+        <div className="flex flex-row row">
+            <div>
+                <Image
+                    className="rounded-md"
+                    src="/doll.jpg"
+                    alt="me, John Doll"
+                    sizes="46vw"
+                    style={{
+                      width: 'auto',
+                      height: '100%',
+                    }}
+                    width={400}
+                    height={600}
+                    priority
+                />
+            </div>
+            <div id="intro-text"> 
+                My name is John Doll and I graduated from Miami University with a Bachelor of Science in Computer Science in May 2023.
+                I started full-time with Lutron Electronics in Boynton Beach, Florida as a Systems Infrastructure Developer in June 2023. I started my pursuit of a Master's 
+                in Computer Science at the University of Illinois in Fall 2024.
+            </div>
         </div>
-        <div id="intro-text"> 
-            My name is John Doll and I graduated from Miami University with a Bachelor of Science in Computer Science in May 2023.
-            I started full-time with Lutron Electronics in Boynton Beach, Florida as a Systems Infrastructure Developer in June 2023. I started my pursuit of a Master's 
-            in Computer Science at the University of Illinois in Fall 2024.
-        </div>
-    </div>
-    {/* make two rows of tiles */}
-    <h1 className="text-7xl">More about me</h1>
+        {/* make two rows of tiles */}
+        <h1 className="text-7xl">More about me</h1>
+      </div>
       <div>
         <h2 className="text-6xl">Projects</h2>
         <div className={styles.tileContainer}>
