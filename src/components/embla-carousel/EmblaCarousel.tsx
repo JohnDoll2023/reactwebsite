65e5a518a33bfbeb28e90e3a5678ad0692c85598
John Doll <jdollmu@gmail.com>
2025-05-17 15:46:47 -0400

    education changes

--- src/components/embla-carousel/EmblaCarousel.tsx
+++ src/components/embla-carousel/EmblaCarousel.tsx
@@ -81,7 +81,9 @@ const EmblaCarousel: React.FC<PropType> = (props) => {
   return (
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
+        <div className={`embla__container ${
+          isEducation ? 'embla__container--education' : ''
+        }`}>
           {slides.map((index) => (
             <div className="embla__slide" key={index}>
               <div className="embla__slide__number">
