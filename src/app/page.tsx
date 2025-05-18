aa33883e8a6511327bdf78f98067ef8b2a259ae0
John Doll <jdollmu@gmail.com>
2025-05-17 22:47:59 -0400

    centered webpage items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,28 +46,34 @@ export default function Home() {
 
   return (
     <main className="flex-1 mt-3">
-      <div className="flex flex-row w-full">
-        <div className="basis-[30%] flex-shrink-0 px-1">
-          <Image
-            className="rounded-md"
-            src="/doll.jpg"
-            alt="me, John Doll"
-            width={600}
-            height={600}
-            priority
-          />
+      <div className="flex flex-col w-full flex-1 min-h-0 md:min-h-[calc(100vh-4rem)]">
+        <div className="flex flex-col md:flex-row w-full flex-1 min-h-0">
+          <div className="flex flex-1 justify-center items-center md:basis-[20%] min-h-0 mx-2">
+            <Image
+              className="rounded-2xl w-auto h-[80vh] object-cover" 
+              src="/doll.jpg"
+              alt="me, John Doll"
+              width={4000}
+              height={6000}
+              priority
+            />
+          </div>
+          <div className="flex-1 text-3xl font-bold font-mono px-2 md:basis-[60%] md:pl-6 flex items-center min-h-0">
+            My name is John Doll and I graduated from Miami University with a B.S. in Computer Science in May 2023.
+            I started full-time with Lutron Electronics in Boynton Beach, Florida as a Systems Infrastructure Developer in June 2023. I started my pursuit of a Master's 
+            in Computer Science at the University of Illinois in Fall 2024.
+          </div>
         </div>
-        <div id="intro-text" className="text-3xl basis-[70%] px-2 font-bold font-mono">
-          My name is John Doll and I graduated from Miami University with a B.S. in Computer Science in May 2023.
-          I started full-time with Lutron Electronics in Boynton Beach, Florida as a Systems Infrastructure Developer in June 2023. I started my pursuit of a Master's 
-          in Computer Science at the University of Illinois in Fall 2024.
-        </div>
+        <h1 className="text-7xl flex-shrink-0 w-full justify-center flex mb-2">
+          More about me
+        </h1>
       </div>
-      {/* make two rows of tiles */}
-      <h1 className="text-7xl">More about me</h1>
       <div>
+        <h2 className="text-5xl">Experience</h2>
         <EmblaCarousel slides={EXPERIENCE_SLIDES} options={OPTIONS} />
+        <h2 className="text-5xl">Projects</h2>
         <EmblaCarousel slides={PROJECT_SLIDES} options={OPTIONS} />
+        <h2 className="text-5xl">Education</h2>
         <EmblaCarousel slides={EDUCATION_SLIDES} options={OPTIONS} />
       </div>
     </main>
