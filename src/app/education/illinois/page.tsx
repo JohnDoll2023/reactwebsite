825568534420fb3564b2b7ab378e01bf55e9cae5
John Doll <jdollmu@gmail.com>
2025-08-03 20:19:11 -0400

    Revert "fix button click"
    This reverts commit a7c2aa1d2e26167794798e0c3a45eb8e5c817652.

--- src/app/education/illinois/page.tsx
+++ src/app/education/illinois/page.tsx
@@ -8,17 +8,8 @@ export const generateMetadata = () => {
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col items-center p-8">
-        <h1 className="text-3xl font-bold mb-6">Illinois</h1>
-        <div className="relative w-full max-w-2xl aspect-video">
-          <Image
-            src="/illinois.png"
-            alt="University of Illinois block I logo"
-            fill
-            className="object-contain"
-            priority
-          />
-        </div>
+    <div className="flex flex-1">
+        Illinois
     </div>
   );
 }
